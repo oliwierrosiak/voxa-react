a70da235f017cea695947d565e04f551fb08cdd4
Oliwier <oliwierrosiak07@gmail.com>
2025-08-12 11:20:59 +0200

    message bar added

---
+++ src/assets/svg/microphone.js
@@ -0,0 +1,8 @@
+function MicrophoneIcon(props)
+{
+    return(
+        <svg className={props.class} viewBox="0 0 32 32"><g id="microphone"><path d="M16,20a5,5,0,0,0,5-5V7A5,5,0,0,0,11,7v8A5,5,0,0,0,16,20ZM13,7a3,3,0,0,1,6,0v8a3,3,0,0,1-6,0Z"/><path d="M25,15a1,1,0,0,0-2,0A7,7,0,0,1,9,15a1,1,0,0,0-2,0,9,9,0,0,0,8,8.94s0,0,0,.06v5a1,1,0,0,0,2,0V24s0,0,0-.06A9,9,0,0,0,25,15Z"/></g></svg>
+    )
+}
+
+export default MicrophoneIcon
--- src/components/chat/chatContent.js
+++ src/components/chat/chatContent.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import styles from './chat.module.css'
 import Loading2 from '../../assets/svg/loading2'
 import refreshToken from '../helpers/refreshToken'
@@ -8,6 +8,12 @@ import { useParams, useNavigate } from 'react-router-dom'
 import ErrorIcon from '../../assets/svg/error'
 import logoutContext from '../context/logoutContext'
 import UserImg from '../homeLogged/userImg'
+import FileIcon from '../../assets/svg/file'
+import MicrophoneIcon from '../../assets/svg/microphone'
+import PhotoIcon from '../../assets/svg/photo'
+import SentIcon from '../../assets/svg/sent'
+import EmoteIcon from '../../assets/svg/emoteIcon'
+import 'emoji-picker-element'
 
 function ChatContent(props)
 {
@@ -15,11 +21,14 @@ function ChatContent(props)
     const [loading,setLoading] = useState(true)
     const [chatError,setChatError] = useState(false)
     const [user,setUser] = useState({})
-
+    const [inputValue,setInputValue] = useState('')
+    const [displayEmoji,setDisplayEmoji] = useState(false)
 
     const navigate = useNavigate()
     const logout = useContext(logoutContext)
     const params = useParams()
+
+    const emoji = useRef()
 
     const getChat = async() =>{
         try
@@ -47,15 +56,34 @@ function ChatContent(props)
     }
 
     useEffect(()=>{
-        console.log(user)
-    },[user])
-
-    useEffect(()=>{
         if(params.id)
         {
             getChat()
+            setDisplayEmoji(false)
+            setInputValue('')
         }
     },[params])
+
+    const emojiClicked = (val) =>{
+        const emoji = val.detail.unicode
+        let value = inputValue+emoji
+        setInputValue(value)
+    }
+
+    useEffect(()=>{
+        if(emoji.current)
+        {
+            emoji.current.addEventListener("emoji-click",emojiClicked)
+
+        }
+        return ()=>{
+            if(emoji.current)
+            {
+                emoji.current.removeEventListener("emoji-click",emojiClicked)
+
+            }
+        }
+    },[inputValue])
 
     return(
         <article className={`${styles.chat} ${props.displayAside?styles.chatReduced:''}`}>
@@ -75,7 +103,28 @@ function ChatContent(props)
                     </header>
                 </div>
             )}
-            <div className={styles.menu}></div>
+            {loading || chatError?null:
+            <div className={styles.menu}>
+                <div className={styles.bottomMenuIcon} onClick={e=>setDisplayEmoji(false)}>
+                    <FileIcon class={styles.bottomMenuSVG}/>
+                </div>
+                <div className={styles.bottomMenuIcon} onClick={e=>setDisplayEmoji(false)}>
+                    <MicrophoneIcon class={styles.bottomMenuSVG}/>
+                </div>
+                <div className={styles.bottomMenuIcon} onClick={e=>setDisplayEmoji(false)}>
+                    <PhotoIcon class={styles.bottomMenuSVG}/>
+                </div>
+                <div className={styles.inputContainer}>
+                    <input type='text' className={styles.input} placeholder='Napisz wiadomość...' onFocus={e=>e.target.placeholder=''} onBlur={e=>e.target.placeholder='Napisz wiadomość...'} value={inputValue} onChange={e=>setInputValue(e.target.value)}/>
+                    <div className={styles.inputIcon} onClick={e=>setDisplayEmoji(!displayEmoji)}>
+                        <EmoteIcon class={styles.inputSVG}/>
+                    </div>
+                    <emoji-picker locale="pl" ref={emoji} className={`${styles.emojiPicker} ${displayEmoji?styles.displayEmoji:''}`}/>
+                </div>
+                <div className={styles.sentIcon} onClick={e=>setDisplayEmoji(false)}>
+                    <SentIcon class={styles.bottomMenuSVG}/>
+                </div>
+            </div>}
         </article>
     )
 }
