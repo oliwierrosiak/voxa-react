c961fb1c2e79f3b7713e20e82e360f436e0eb559
Oliwier <oliwierrosiak07@gmail.com>
2025-08-20 13:24:58 +0200

    Gallery done

--- src/components/chat/galleryImg.js
+++ src/components/chat/galleryImg.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import refreshToken from "../helpers/refreshToken"
 import axios from "axios"
 import ApiAddress from "../../ApiAddress"
@@ -12,6 +12,18 @@ function GalleryImg(props)
     const [url,setUrl] = useState('')
     const [error,setError] = useState(false)
     const [video,setVideo] = useState(false)
+    const [zoom,setZoom] = useState(1)
+    const [dragging,setDragging] = useState(false)
+    const [positions,setPositions] = useState({
+        x:0,
+        y:0,
+    })
+    const [start,setStart] = useState({
+        x:0,
+        y:0,
+    })
+
+    const imgRef = useRef()
 
     const getPhoto = async() =>{
         try
@@ -43,19 +55,118 @@ function GalleryImg(props)
 
     }
 
+    const zoomImg = (e) =>
+    {
+        if(e.deltaY<0)
+        {
+            if(zoom<15)
+            {
+                setZoom(zoom+0.5)
+            }
+        }
+        else
+        {
+            if(zoom>1)
+            {
+                setZoom(zoom-0.5)
+                setStart({x:0,y:0})
+                setPositions({x:0,y:0})
+                
+            }
+        }
+    }
+
+    const handleMouseDown = (e) => {
+        e.preventDefault();
+        if(zoom > 1)
+        {
+            setDragging(true);
+            setStart({
+            x: e.clientX - positions.x,
+            y: e.clientY - positions.y,
+            });
+        }
+    };
+
+  const handleMouseMove = (e) => {
+    if (dragging)
+    {
+        const maxWidth = (imgRef.current.clientWidth * zoom - imgRef.current.clientWidth)/2
+        const maxHeight = (imgRef.current.clientHeight * zoom -imgRef.current.clientHeight)/2
+
+        let localX = 0
+        let localY = 0
+        if(Math.abs(e.clientX - start.x) < maxWidth)
+        {
+            localX = e.clientX - start.x
+        }
+        else
+        {
+            localX = positions.x
+        }
+
+        if(Math.abs(e.clientY - start.y) < maxHeight)
+        {
+            localY = e.clientY - start.y
+        }
+        else
+        {
+            localY = positions.y
+        }
+
+        setPositions({
+            x: localX,
+            y: localY,
+        });
+    }
+    
+  };
+
+  const handleMouseUp = () => {
+    setDragging(false);
+  };
+
+    useEffect(()=>{
+        if(imgRef.current)
+        {
+            if(zoom>1)
+            {
+                imgRef.current.style.cursor = `grab`
+            }
+            else
+            {
+                imgRef.current.style.cursor = `default`
+            }
+        }
+    },[zoom])
+
     useEffect(()=>{
         setLoading(true)
         setError(false)
         getPhoto()
         setVideo(false)
         checkExtension()
+
+    setZoom(1)
+   setDragging(false)
+   setPositions({
+        x:0,
+        y:0,
+    })
+    setStart({
+        x:0,
+        y:0,
+    })
+
     },[props])
 
     return(
         loading?<Loading2 class={styles.galleryLoading} />:error?<div className={styles.photoError}>
             <ErrorIcon />
             <h2>Błąd pobierania</h2>
-        </div>:video?<video controls src={url} className={styles.galleryVideo}/>:<img src={url} className={styles.galleryImg}/>
+        </div>:video?<video controls src={url} className={styles.galleryVideo}/>:<div className={styles.galleryImgContainer} onWheel={zoomImg}><img src={url} draggable={false} style={{
+          transform: `translate(${positions.x}px, ${positions.y}px) scale(${zoom})`
+        }} className={styles.galleryImg} ref={imgRef} onMouseMove={handleMouseMove} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}/></div>
     )
 }
 
